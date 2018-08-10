fbc2d02632060c58b2a0e0f17f3122ae42489806
ableigdev <igor.yarovoy.98@ukr.net>
2018-08-10 11:15:56 +0300

    Add a code for testing libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "git2.h"
+#include "mainwindow.h"
+#include <QApplication>
 
-int main()
+int main(int argc, char *argv[])
 {
-	return 0;
+    QApplication a(argc, argv);
+    MainWindow w;
+    git_libgit2_init();
+    git_repository *repo = NULL;
+    const char *url = "https://github.com/pablospe/cmake-example-library.git";
+    const char *path = "/home/igdev/fl";
+    int error = git_clone(&repo, url, path, NULL);
+    std:: cout << error << std::endl;
+    w.show();
+
+    return a.exec();
 }
