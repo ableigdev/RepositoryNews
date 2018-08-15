9b46aa4dbab87d5ec368336265bb0c472df3109b
ableigdev <igor.yarovoy.98@ukr.net>
2018-08-15 12:42:00 +0300

    Add a variable which stores a path to Qt; include Qt libraries

--- cmake/FindQt.cmake
+++ cmake/FindQt.cmake
@@ -1,4 +1,6 @@
-set(CMAKE_PREFIX_PATH /home/igdev/Qt/5.10.0/gcc_64/)
+set(PATH_TO_QT "/path/to/your/Qt/" CACHE STRING "Path to Qt")
+message("-- Path to Qt(CMake): " ${PATH_TO_QT})
+set(CMAKE_PREFIX_PATH ${PATH_TO_QT})
 # Find includes in corresponding build directories
 set(CMAKE_INCLUDE_CURRENT_DIR ON)
 # Instruct CMake to run moc automatically when needed
@@ -8,3 +10,6 @@ set(CMAKE_AUTOUIC ON)
 
 # Find the QtWidgets library
 find_package(Qt5Widgets CONFIG REQUIRED)
+
+include_directories(${Qt5Widgets_INCLUDE_DIRS})
+add_definitions(${Qt5Widgets_DEFINITIONS})
