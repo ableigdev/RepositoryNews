bc8861b76407117e2e771566a6497d36ab642520
ableigdev <igor.yarovoy.98@ukr.net>
2018-08-15 12:43:31 +0300

    Add instruction for build and install the main project

--- build.sh
+++ build.sh
@@ -4,3 +4,9 @@ mkdir build
 cd build
 cmake -DBUILD_CLAR=OFF -DCMAKE_INSTALL_PREFIX=../../installed ..
 cmake --build . --target install
+cd ../../
+mkdir build
+cd build
+echo "-- Path to qt(bash): " $1
+cmake -DPATH_TO_QT:STRING=$1 -DCMAKE_INSTALL_PREFIX=../rpnews_installed ..
+cmake --build . --target install
