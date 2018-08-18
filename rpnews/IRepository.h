5c4f88f9c75de164b3847033105b4cace2934d7d
ableigdev <igor.yarovoy.98@ukr.net>
2018-08-18 22:36:30 +0300

    Add the first version of interface for the repository

---
+++ rpnews/IRepository.h
@@ -0,0 +1,11 @@
+#ifndef RPNEWS_IREPOSITORY_H
+#define RPNEWS_IREPOSITORY_H
+
+class IRepository
+{
+public:
+    IRepository() = default;
+    ~IRepository() = default;
+};
+
+#endif //RPNEWS_IREPOSITORY_H
