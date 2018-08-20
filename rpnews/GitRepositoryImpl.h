7097c1140ed5b1cad2a89a3a9df540afdc8f5801
ableigdev <igor.yarovoy.98@ukr.net>
2018-08-20 21:55:07 +0300

    Add an interface for the implementation git repository

---
+++ rpnews/GitRepositoryImpl.h
@@ -0,0 +1,14 @@
+#ifndef RPNEWS_GITREPOSITORYIMPL_H
+#define RPNEWS_GITREPOSITORYIMPL_H
+
+#include "IRepository.h"
+
+class GitRepositoryImpl : public IRepository
+{
+public:
+    GitRepositoryImpl() = default;
+    virtual ~GitRepositoryImpl() = default;
+};
+
+
+#endif //RPNEWS_GITREPOSITORYIMPL_H
