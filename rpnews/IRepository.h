5fb84c652d77be35a155f45115df351dd82cc13e
ableigdev <igor.yarovoy.98@ukr.net>
2018-08-21 22:09:53 +0300

    Update an interface

--- rpnews/IRepository.h
+++ rpnews/IRepository.h
@@ -1,11 +1,16 @@
 #ifndef RPNEWS_IREPOSITORY_H
 #define RPNEWS_IREPOSITORY_H
 
+#include <vector>
+#include <string>
+
 class IRepository
 {
 public:
     IRepository() = default;
-    ~IRepository() = default;
+    virtual ~IRepository() = default;
+    virtual std::vector<std::string> getBranchName() const = 0;
+    virtual size_t getNumberOfBranches() const = 0;
 };
 
 #endif //RPNEWS_IREPOSITORY_H
