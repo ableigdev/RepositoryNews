8049319582b57e65a1d90c7298b20b589d771e07
ableigdev <igor.yarovoy.98@ukr.net>
2018-08-22 23:49:58 +0300

    Add the first version an interface for the data type

---
+++ rpnews/IRepositoryType.h
@@ -0,0 +1,11 @@
+#ifndef RPNEWS_IREPOSITORYTYPE_H
+#define RPNEWS_IREPOSITORYTYPE_H
+
+class IRepositoryType
+{
+public:
+    IRepositoryType() = default;
+    virtual ~IRepositoryType() = default;
+};
+
+#endif //RPNEWS_IREPOSITORYTYPE_H
