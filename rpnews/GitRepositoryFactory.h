51b1bb311404c8523a3bd99cbc2740a788e6fd24
ableigdev <igor.yarovoy.98@ukr.net>
2018-08-30 23:15:05 +0300

    Add parameters for the method

--- rpnews/GitRepositoryFactory.h
+++ rpnews/GitRepositoryFactory.h
@@ -8,7 +8,7 @@ class GitRepositoryFactory : public IRepositoryFactory
 public:
     GitRepositoryFactory() = default;
     virtual ~GitRepositoryFactory() = default;
-    virtual IRepository* createRepository();
+    virtual IRepository* createRepository(const std::string& url, const secure_string& user, const secure_string& pass);
 };
 
 #endif //RPNEWS_GITREPOSITORYFACTORY_H
--- rpnews/IRepositoryFactory.h
+++ rpnews/IRepositoryFactory.h
@@ -1,13 +1,15 @@
 #ifndef RPNEWS_IREPOSITORYFACTORY_H
 #define RPNEWS_IREPOSITORYFACTORY_H
 
+#include <string>
 #include "IRepository.h"
+#include "rpnews_types.h"
 
 class IRepositoryFactory
 {
 public:
     IRepositoryFactory() = default;
     virtual ~IRepositoryFactory() = default;
-    virtual IRepository* createRepository() = 0;
+    virtual IRepository* createRepository(const std::string& url, const secure_string& user, const secure_string& pass) = 0;
 };
 #endif //RPNEWS_IREPOSITORYFACTORY_H
