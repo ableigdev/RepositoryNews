2c751ef52043985f17076935988d5b12209c38b5
ableigdev <igor.yarovoy.98@ukr.net>
2018-09-01 14:23:58 +0300

    Add 2 methods to the interface

--- rpnews/IRepositoryType.h
+++ rpnews/IRepositoryType.h
@@ -1,11 +1,16 @@
 #ifndef RPNEWS_IREPOSITORYTYPE_H
 #define RPNEWS_IREPOSITORYTYPE_H
 
+#include <vector>
+#include <string>
+
 class IRepositoryType
 {
 public:
     IRepositoryType() = default;
     virtual ~IRepositoryType() = default;
+    virtual std::vector<std::string> getNameOfBranches() const = 0;
+    virtual size_t getNumberOfBranches() const = 0;
 };
 
 #endif //RPNEWS_IREPOSITORYTYPE_H
