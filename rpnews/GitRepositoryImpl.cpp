3ed3a53b798d1634ff50a64c266c9df0db303d55
ableigdev <igor.yarovoy.98@ukr.net>
2018-09-02 20:30:11 +0300

    Update an interface and implement methods

--- rpnews/GitRepositoryImpl.cpp
+++ rpnews/GitRepositoryImpl.cpp
@@ -1,2 +1,23 @@
 #include "GitRepositoryImpl.h"
 
+GitRepositoryImpl::GitRepositoryImpl(const std::string& url, const secure_string& user, const secure_string& pass)
+: m_GitRepositoryTypeImpl(url, user, pass)
+{
+
+}
+
+std::vector<std::string> GitRepositoryImpl::getBranchName() const
+{
+    return m_GitRepositoryTypeImpl.getNameOfBranches();
+}
+
+size_t GitRepositoryImpl::getNumberOfBranches() const
+{
+    return m_GitRepositoryTypeImpl.getNumberOfBranches();
+}
+
+void GitRepositoryImpl::setCurrentBranch(size_t index)
+{
+    // TODO: Finish this method
+}
+
--- rpnews/GitRepositoryImpl.h
+++ rpnews/GitRepositoryImpl.h
@@ -2,12 +2,20 @@
 #define RPNEWS_GITREPOSITORYIMPL_H
 
 #include "IRepository.h"
+#include "SecureAllocator.h"
+#include "GitRepositoryTypeImpl.h"
 
 class GitRepositoryImpl : public IRepository
 {
 public:
-    GitRepositoryImpl() = default;
+    GitRepositoryImpl(const std::string& url, const secure_string& user, const secure_string& pass);
     virtual ~GitRepositoryImpl() = default;
+    virtual std::vector<std::string> getBranchName() const;
+    virtual size_t getNumberOfBranches() const;
+    virtual void setCurrentBranch(size_t);
+
+private:
+    GitRepositoryTypeImpl m_GitRepositoryTypeImpl;
 };
 
 
