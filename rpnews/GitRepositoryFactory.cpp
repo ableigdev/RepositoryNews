53a4207e5c763dc5cf7281757ca94e0df461bf0a
ableigdev <igor.yarovoy.98@ukr.net>
2018-09-03 21:35:36 +0300

    Implement the 'createRepository' method

--- rpnews/GitRepositoryFactory.cpp
+++ rpnews/GitRepositoryFactory.cpp
@@ -1,1 +1,7 @@
 #include "GitRepositoryFactory.h"
+#include "GitRepositoryImpl.h"
+
+IRepository* GitRepositoryFactory::createRepository(const std::string& url, const secure_string& user, const secure_string& pass) 
+{
+    return new GitRepositoryImpl(url, user, pass);
+}
