dfaed1b75e54706011411fdb812c40d2427017bf
ableigdev <igor.yarovoy.98@ukr.net>
2018-09-07 23:03:53 +0300

    Add a function which removes config folder

--- tests/CreateNewGitRepository/CreateNewGitRepository.cpp
+++ tests/CreateNewGitRepository/CreateNewGitRepository.cpp
@@ -1,4 +1,28 @@
 #include "gtest/gtest.h"
+#include <QDir>
+#include <string>
+#include <memory>
+
+#include "rpnews/IRepository.h"
+#include "rpnews/IRepositoryType.h"
+#include "rpnews/IRepositoryFactory.h"
+#include "rpnews/GitRepositoryTypeImpl.h"
+#include "rpnews/GitRepositoryImpl.h"
+#include "rpnews/GitRepositoryFactory.h"
+
+namespace
+{
+    void deleteFolder()
+    {
+        QDir dir;
+        dir.setFilter(QDir::Files | QDir::Hidden | QDir::NoSymLinks);
+        if (dir.exists(".configs"))
+        {
+            dir.cd(".configs");
+            dir.removeRecursively();
+        }
+    }
+}
 
 TEST(Test1, Valid)
 {
