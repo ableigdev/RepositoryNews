3692bf857b2df665307ea93e58e3cd1f8c2c108f
ableigdev <igor.yarovoy.98@ukr.net>
2018-10-04 18:15:57 +0300

    Begin to implement adding a new repository

--- rpnews/gui/mainwindow.cpp
+++ rpnews/gui/mainwindow.cpp
@@ -1,14 +1,17 @@
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
 #include <QStyle>
 #include <QDesktopWidget>
 #include <QApplication>
+#include <QDebug>
+#include <QMessageBox>
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "rpnews/git_implementation/GitRepositoryFactory.h"
 
-MainWindow::MainWindow(QWidget *parent) :
+MainWindow::MainWindow(QWidget* parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    m_UI(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+    m_UI->setupUi(this);
     this->setGeometry(
                 QStyle::alignedRect(
                     Qt::LeftToRight,
@@ -17,11 +20,13 @@ MainWindow::MainWindow(QWidget *parent) :
                     qApp->desktop()->availableGeometry()
                     )
                 );
+    initializeComboBoxRepositoryType();
+    on_ComboBox_RepositoryType_activated(0);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+    delete m_UI;
 }
 
 void MainWindow::closeEvent(QCloseEvent* event)
@@ -30,5 +35,38 @@ void MainWindow::closeEvent(QCloseEvent* event)
     {
         event->ignore();
         this->hide();
+    }
+}
+
+void MainWindow::initializeComboBoxRepositoryType()
+{
+    m_UI->ComboBox_RepositoryType->addItem("Git");
+}
+
+void MainWindow::on_ComboBox_RepositoryType_activated(int index)
+{
+    switch (index)
+    {
+        case 0:
+        {
+            m_RepositoryFactory = std::make_unique<GitRepositoryFactory>();
+            break;
+        }
+        default:
+        {
+            break;
+        }
+    }
+}
+
+void MainWindow::on_Button_Connect_clicked()
+{
+    if (m_UI->Edit_URL->text().isEmpty() || m_UI->Edit_Login->text().isEmpty() || m_UI->Edit_Password->text().isEmpty())
+    {
+        QMessageBox::warning(this, "Warning", "Input your credentials!", QMessageBox::Ok);
+    }
+    else
+    {
+
     }
 }
--- rpnews/gui/mainwindow.h
+++ rpnews/gui/mainwindow.h
@@ -3,9 +3,12 @@
 
 #include <QMainWindow>
 #include <QCloseEvent>
+#include <memory>
+#include "rpnews/interfaces/IRepositoryFactory.h"
 
-namespace Ui {
-class MainWindow;
+namespace Ui
+{
+    class MainWindow;
 }
 
 class MainWindow : public QMainWindow
@@ -13,14 +16,22 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_Button_Connect_clicked();
+
+private slots:
+    void on_ComboBox_RepositoryType_activated(int index);
 
 private:
     void closeEvent(QCloseEvent* event);
+    void initializeComboBoxRepositoryType();
 
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow* m_UI;
+    std::unique_ptr<IRepositoryFactory> m_RepositoryFactory;
 };
 
 #endif // MAINWINDOW_H
