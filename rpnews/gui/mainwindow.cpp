e84ddd6bfb6f54ad54629e8f77b4e54c119d50a4
ableigdev <igor.yarovoy.98@ukr.net>
2018-10-06 00:24:30 +0300

    Treatment of the exceptions; Add slots for the form controls

--- rpnews/gui/mainwindow.cpp
+++ rpnews/gui/mainwindow.cpp
@@ -3,9 +3,12 @@
 #include <QApplication>
 #include <QDebug>
 #include <QMessageBox>
+#include <QDir>
+#include <stdexcept>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "rpnews/git_implementation/GitRepositoryFactory.h"
+#include "rpnews/helpers/ConfigChecker.h"
 
 MainWindow::MainWindow(QWidget* parent) :
     QMainWindow(parent),
@@ -61,12 +64,106 @@ void MainWindow::on_ComboBox_RepositoryType_activated(int index)
 
 void MainWindow::on_Button_Connect_clicked()
 {
-    if (m_UI->Edit_URL->text().isEmpty() || m_UI->Edit_Login->text().isEmpty() || m_UI->Edit_Password->text().isEmpty())
+    auto url = m_UI->Edit_URL->text().toStdString();
+    auto login = m_UI->Edit_Login->text().toStdString();
+    auto pass = m_UI->Edit_Password->text().toStdString();
+    std::string folderName;
+
+    if (url.empty() || login.empty() || pass.empty())
     {
         QMessageBox::warning(this, "Warning", "Input your credentials!", QMessageBox::Ok);
     }
     else
     {
-
+        folderName = ConfigChecker::getRepositoryFolderName(url);
+        try
+        {
+            m_Repository.reset(m_RepositoryFactory->createRepository(url, login.c_str(), pass.c_str(), false));
+            m_Repository->prepareRepository();
+            m_Repository->prepareBranches();
+            initializeComboBoxBranchName();
+            on_ComboBox_BranchName_activated(0);
+            initializeComboBoxTimeInterval();
+            on_ComboBox_TimeInterval_activated(0);
+            m_UI->Button_Add_Save->setEnabled(true);
+            m_UI->Button_Connect->setEnabled(false);
+        }
+        catch (const std::logic_error& e)
+        {
+            deleteRepositoryFolder(folderName);
+            QMessageBox::critical(this, "Error", e.what(), QMessageBox::Critical, QMessageBox::Ok);
+        }
     }
+}
+
+void MainWindow::deleteRepositoryFolder(const std::string& name)
+{
+    QDir dir;
+    dir.setFilter(QDir::Files | QDir::Hidden | QDir::NoSymLinks);
+
+    if (dir.current().exists(".configs"))
+    {
+        dir.cd(".configs");
+        dir.cd(name.c_str());
+        dir.removeRecursively();
+    }
+}
+
+void MainWindow::initializeComboBoxBranchName()
+{
+    auto nameOfBranches = m_Repository->getBranchName();
+    for (const auto& i : nameOfBranches)
+    {
+        m_UI->ComboBox_BranchName->addItem(i.c_str());
+    }
+    m_UI->ComboBox_BranchName->setEnabled(true);
+}
+
+void MainWindow::initializeComboBoxTimeInterval()
+{
+    m_UI->ComboBox_TimeInterval->addItem("5 minutes");
+    m_UI->ComboBox_TimeInterval->addItem("30 minutes");
+    m_UI->ComboBox_TimeInterval->addItem("1 hour");
+    m_UI->ComboBox_TimeInterval->setEnabled(true);
+}
+
+void MainWindow::on_ComboBox_BranchName_activated(int index)
+{
+    m_Repository->setCurrentBranch(static_cast<size_t>(index));
+}
+
+void MainWindow::on_ComboBox_TimeInterval_activated(int index)
+{
+    switch (index)
+    {
+        case 0:
+        {
+            std::chrono::minutes min {5};
+            m_TimeForSynchronization = min;
+            break;
+        }
+        case 1:
+        {
+            std::chrono::minutes min {30};
+            m_TimeForSynchronization = min;
+            break;
+        }
+        case 2:
+        {
+            std::chrono::hours hours {1};
+            m_TimeForSynchronization = hours;
+            break;
+        }
+    }
+}
+
+void MainWindow::on_Button_Add_Save_clicked()
+{
+    m_Repository->saveConfig();
+    this->close();
+}
+
+void MainWindow::on_Button_Cancel_clicked()
+{
+    this->close();
 }
--- rpnews/gui/mainwindow.h
+++ rpnews/gui/mainwindow.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 #include <QCloseEvent>
 #include <memory>
+#include <chrono>
 #include "rpnews/interfaces/IRepositoryFactory.h"
 
 namespace Ui
@@ -20,18 +21,25 @@ public:
     ~MainWindow();
 
 private slots:
+    void on_Button_Cancel_clicked();
+    void on_Button_Add_Save_clicked();
+    void on_ComboBox_TimeInterval_activated(int index);
+    void on_ComboBox_BranchName_activated(int index);
     void on_Button_Connect_clicked();
-
-private slots:
     void on_ComboBox_RepositoryType_activated(int index);
 
 private:
     void closeEvent(QCloseEvent* event);
     void initializeComboBoxRepositoryType();
+    void deleteRepositoryFolder(const std::string& name);
+    void initializeComboBoxBranchName();
+    void initializeComboBoxTimeInterval();
 
 private:
     Ui::MainWindow* m_UI;
     std::unique_ptr<IRepositoryFactory> m_RepositoryFactory;
+    std::unique_ptr<IRepository> m_Repository;
+    std::chrono::seconds m_TimeForSynchronization;
 };
 
 #endif // MAINWINDOW_H
