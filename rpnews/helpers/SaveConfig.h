e6c8e74c7e98a125c7979e53fae87865892bdf59
ableigdev <igor.yarovoy.98@ukr.net>
2018-10-12 19:15:02 +0300

    Add the method which saves config of the GUI

--- rpnews/helpers/SaveConfig.h
+++ rpnews/helpers/SaveConfig.h
@@ -2,6 +2,7 @@
 #define RPNEWS_SAVECONFIG_H
 
 #include <string>
+#include <chrono>
 #include "rpnews_types.h"
 
 class SaveConfig
@@ -10,6 +11,7 @@ public:
     SaveConfig() = default;
     ~SaveConfig() = default;
     static void save(const std::string& path, const TypeRepository& type, const secure_string& user, const secure_string& pass);
+    static void saveGUIConfig(const std::string& path, const std::string& branchName, size_t branchIndex, std::chrono::seconds time);
 };
 
 #endif //RPNEWS_SAVECONFIG_H
