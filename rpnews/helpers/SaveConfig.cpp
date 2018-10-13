059a8165c774d99c802b20420143a9b2b6944376
ableigdev <igor.yarovoy.98@ukr.net>
2018-10-13 11:55:58 +0300

    Implement the method which saves gui config

--- rpnews/helpers/SaveConfig.cpp
+++ rpnews/helpers/SaveConfig.cpp
@@ -7,3 +7,10 @@ void SaveConfig::save(const std::string& path, const TypeRepository& type, const
     outFile << type << " " << user << " " << pass;
     outFile.close();
 }
+
+void SaveConfig::saveGUIConfig(const std::string& path, const std::string& branchName, size_t branchIndex, std::chrono::seconds time)
+{
+    std::ofstream outFile(path + "config_gui_rep", std::ios::out);
+    outFile << branchName << " " << branchIndex << " " << time.count();
+    outFile.close();
+}
