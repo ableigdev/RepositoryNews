7dc90e99263c40d7ee482f6b90904c79ad90246d
ableigdev <igor.yarovoy.98@ukr.net>
2018-11-03 15:20:47 +0200

    Add a common function which returns time interval

---
+++ rpnews/helpers/GetTimeInterval.h
@@ -0,0 +1,10 @@
+#ifndef GETTIMEINTERVAL_H
+#define GETTIMEINTERVAL_H
+
+#include <chrono>
+
+namespace helpers
+{
+    std::chrono::seconds getTimeInterval(int index);
+}
+#endif // GETTIMEINTERVAL_H
