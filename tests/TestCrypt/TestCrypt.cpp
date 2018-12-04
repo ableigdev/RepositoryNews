df612557c04e2f4ad9c3a39ef2a9fd0f80c559ab
ableigdev <igor.yarovoy.98@ukr.net>
2018-12-04 22:45:22 +0200

    Prepare data for the testing

--- tests/TestCrypt/TestCrypt.cpp
+++ tests/TestCrypt/TestCrypt.cpp
@@ -1,4 +1,19 @@
 #include "gtest/gtest.h"
+#include "rpnews/helpers/AESEncryption.h"
+#include <QByteArray>
+
+namespace
+{
+    QByteArray key(QByteArray::fromHex("000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F"));
+    QByteArray plainText(QByteArray::fromHex("00112233445566778899AABBCCDDEEFF"));
+    QByteArray expectedResult(QByteArray::fromHex("8EA2B7CA516745BFEAFC49904B496089"));
+}
+
+
+TEST(Encrypt, Valid)
+{
+    EXPECT_EQ(AESEncryption::encrypt(plainText, key), expectedResult);
+}
 
 int main(int argc, char** argv)
 {
