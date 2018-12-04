f475d3942257dbf50a1cbd64511cc24133fc0f0e
ableigdev <igor.yarovoy.98@ukr.net>
2018-12-05 00:19:08 +0200

    Add unit tests for the crypt class

--- tests/TestCrypt/TestCrypt.cpp
+++ tests/TestCrypt/TestCrypt.cpp
@@ -1,18 +1,32 @@
 #include "gtest/gtest.h"
 #include "rpnews/helpers/AESEncryption.h"
-#include <QByteArray>
 
 namespace
 {
     QByteArray key(QByteArray::fromHex("000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F"));
     QByteArray plainText(QByteArray::fromHex("00112233445566778899AABBCCDDEEFF"));
-    QByteArray expectedResult(QByteArray::fromHex("8EA2B7CA516745BFEAFC49904B496089"));
 }
 
-
-TEST(Encrypt, Valid)
+TEST(AESEncrypt, Valid)
 {
-    EXPECT_EQ(AESEncryption::encrypt(plainText, key), expectedResult);
+    auto result = AESEncryption::encrypt(plainText, key);
+    EXPECT_EQ(AESEncryption::decrypt(result, key), plainText);
+}
+
+TEST(AESEncrypt, Valid2)
+{
+    std::string key("kasjdfhkl234234");
+    std::string text("test");
+    auto result = AESEncryption::encrypt(text.data(), key.data());
+    EXPECT_EQ(AESEncryption::decrypt(result, key.data()).data(), text);
+}
+
+TEST(ASEEncrypt, Invalid)
+{
+    std::string key("kasjdfhkl234234");
+    std::string text("test");
+    auto result = AESEncryption::encrypt(text.data(), key.data());
+    EXPECT_NE(AESEncryption::decrypt(result, key.substr(0, 4).data()).data(), text);
 }
 
 int main(int argc, char** argv)
