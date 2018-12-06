1bb320e2d4745bd6b26fb2e8425d7e2eb0026a49
ableigdev <igor.yarovoy.98@ukr.net>
2018-12-06 17:26:05 +0200

    Remove unuseful font

--- rpnews/main.cpp
+++ rpnews/main.cpp
@@ -7,12 +7,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setQuitOnLastWindowClosed(false);
-    if (QFontDatabase::addApplicationFont(":/fonts/fonts/OpenSans-Regular.ttf") != -1)
-    {
-        QFont font("OpenSans-Regular");
-        font.setPointSize(10);
-        a.setFont(font);
-    }
     TrayIconApp app;
 
     return a.exec();
