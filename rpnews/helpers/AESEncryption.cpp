d6b3ea0d222dba5deb33c21a8c4bc23b13fd6998
ableigdev <igor.yarovoy.98@ukr.net>
2018-12-06 16:55:39 +0200

    Refactor the 'AESEncryption' class
    Change a lib which AES algorithm was based

--- rpnews/helpers/AESEncryption.cpp
+++ rpnews/helpers/AESEncryption.cpp
@@ -1,69 +1,28 @@
 #include "AESEncryption.h"
-#include <openssl/evp.h>
-#include <openssl/aes.h>
+#include "cryptopp/hex.h"
+#include "cryptopp/filters.h"
+#include "cryptopp/aes.h"
+#include "cryptopp/modes.h"
+#include <vector>
 
-QByteArray AESEncryption::sha256(const QByteArray& text)
+std::string AESEncryption::encrypt(const std::string& data, const std::string& password)
 {
-    unsigned int outLen = 0;
-    QByteArray dataBuff;
-    dataBuff.resize(EVP_MAX_MD_SIZE);
-    #if OPENSSL_VERSION_NUMBER < 0x10100000L
-        EVP_MD_CTX* evpMdCtx = EVP_MD_CTX_create();
-    #else
-        EVP_MD_CTX* evpMdCtx = EVP_MD_CTX_new();
-    #endif
-    EVP_DigestInit(evpMdCtx, EVP_sha256());
-    EVP_DigestUpdate(evpMdCtx, text.data(), static_cast<size_t>(text.size()));
-    EVP_DigestFinal_ex(evpMdCtx, reinterpret_cast<unsigned char*>(dataBuff.data()), &outLen);
-    #if OPENSSL_VERSION_NUMBER < 0x10100000L
-        EVP_MD_CTX_cleanup(evpMdCtx);
-        OPENSSL_free(evpMdCtx);
-    #else
-        EVP_MD_CTX_free(evpMdCtx);
-    #endif
-    dataBuff.resize(static_cast<int>(outLen));
-    return dataBuff.toHex();
+    std::vector<CryptoPP::byte> key(password.begin(), password.end());
+    std::string result;
+    CryptoPP::ECB_Mode<CryptoPP::AES>::Encryption encryption;
+    encryption.SetKey(key.data(), key.size());
+    CryptoPP::StringSource(data, true, new CryptoPP::StreamTransformationFilter(encryption, new CryptoPP::StringSink(result)));
+
+    return result;
 }
 
-QByteArray AESEncryption::encrypt(const QByteArray& data, const QByteArray& password)
+std::string AESEncryption::decrypt(const std::string& data, const std::string& password)
 {
-    int outLen = 0;
-    QByteArray dataBuff;
-    dataBuff.resize(data.size() + AES_BLOCK_SIZE);
-    EVP_CIPHER_CTX* evpCipherCtx = EVP_CIPHER_CTX_new();
-    EVP_CIPHER_CTX_init(evpCipherCtx);
-    EVP_EncryptInit(evpCipherCtx, EVP_aes_256_cbc(), reinterpret_cast<const unsigned char*>(sha256(password).data()),
-                    reinterpret_cast<const unsigned char*>(sha256(password).data()));
-    EVP_EncryptUpdate(evpCipherCtx, reinterpret_cast<unsigned char*>(dataBuff.data()), &outLen,
-                      reinterpret_cast<const unsigned char*>(data.data()),
-                      data.size());
-    int tempLen = outLen;
-    EVP_EncryptFinal(evpCipherCtx, reinterpret_cast<unsigned char*>(dataBuff.data()) + tempLen, &outLen);
-    tempLen += outLen;
-    EVP_CIPHER_CTX_cleanup(evpCipherCtx);
-    EVP_CIPHER_CTX_free(evpCipherCtx);
-    dataBuff.resize(tempLen);
-    return dataBuff;
-}
-
-QByteArray AESEncryption::decrypt(const QByteArray& data, const QByteArray& password)
-{
-    int outLen = 0;
-    QByteArray dataBuff;
-    dataBuff.resize(data.size() + AES_BLOCK_SIZE);
-    EVP_CIPHER_CTX* evpCipherCtx = EVP_CIPHER_CTX_new();
-    EVP_CIPHER_CTX_init(evpCipherCtx);
-    EVP_DecryptInit(evpCipherCtx, EVP_aes_256_cbc(), reinterpret_cast<const unsigned char*>(sha256(password).data()),
-                    reinterpret_cast<const unsigned char*>(sha256(password).data()));
-    EVP_DecryptUpdate(evpCipherCtx, reinterpret_cast<unsigned char*>(dataBuff.data()), &outLen,
-                      reinterpret_cast<const unsigned char*>(data.data()),
-                      data.size());
-    int tempLen = outLen;
-    EVP_DecryptFinal(evpCipherCtx, reinterpret_cast<unsigned char*>(dataBuff.data()) + tempLen, &outLen);
-    tempLen += outLen;
-    EVP_CIPHER_CTX_cleanup(evpCipherCtx);
-    EVP_CIPHER_CTX_free(evpCipherCtx);
-    dataBuff.resize(tempLen);
-    return dataBuff;
+    std::vector<CryptoPP::byte> key(password.begin(), password.end());
+    std::string result;
+    CryptoPP::ECB_Mode<CryptoPP::AES>::Decryption decryption;
+    decryption.SetKey(key.data(), key.size());
+    CryptoPP::StringSource(data, true, new CryptoPP::StreamTransformationFilter(decryption, new CryptoPP::StringSink(result)));
+    return result;
 }
 
--- rpnews/helpers/AESEncryption.h
+++ rpnews/helpers/AESEncryption.h
@@ -1,16 +1,13 @@
 #ifndef AESENCRYPTION_H
 #define AESENCRYPTION_H
 
-#include <QBitArray>
+#include <string>
 
 class AESEncryption
 {
 public:
-    static QByteArray decrypt(const QByteArray& data, const QByteArray& password);
-    static QByteArray encrypt(const QByteArray& data, const QByteArray& password);
-
-private:
-    static QByteArray sha256(const QByteArray& text);
+    static std::string decrypt(const std::string& data, const std::string& password);
+    static std::string encrypt(const std::string& data, const std::string& password);
 };
 
 #endif // AESENCRYPTION_H
