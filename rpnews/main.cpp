ce344fed6584d7ffbd96108a29b626573182822f
ableigdev <igor.yarovoy.98@ukr.net>
2018-12-15 17:32:38 +0200

    Add a property for the running to one instance of the application

--- rpnews/main.cpp
+++ rpnews/main.cpp
@@ -1,11 +1,24 @@
 #include <QApplication>
+#include <QLockFile>
+#include <QDir>
+#include <QMessageBox>
 #include "gui/trayiconapp.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setQuitOnLastWindowClosed(false);
-    TrayIconApp app;
+    QLockFile lockFile(QDir::temp().absoluteFilePath("temp.lock"));
 
+    if (!lockFile.tryLock(100))
+    {
+        QMessageBox message;
+        message.setIcon(QMessageBox::Warning);
+        message.setText("The application was run.\nYou can run only one instance of the application.");
+        message.exec();
+        return 1;
+    }
+
+    TrayIconApp app;
     return a.exec();
 }
