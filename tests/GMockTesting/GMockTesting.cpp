4407639684352f057b8ce76ac5fe35f88b0429b2
ableigdev <igor.yarovoy.98@ukr.net>
2018-12-19 21:55:10 +0200

    Add the first version of the gmock test

--- tests/GMockTesting/GMockTesting.cpp
+++ tests/GMockTesting/GMockTesting.cpp
@@ -1,4 +1,23 @@
 #include "gmock/gmock.h"
+#include "rpnews/git_implementation/GitRepositoryImpl.h"
+#include "rpnews/git_implementation/GitRepositoryFactory.h"
+#include "rpnews/git_implementation/GitRepositoryTypeImpl.h"
+#include "rpnews/helpers/rpnews_types.h"
+
+using namespace testing;
+
+class MockGitRepositoryFactory
+{
+public:
+    MOCK_METHOD4(createRepository, IRepository*(const std::string& url, const secure_string& user, const secure_string& pass, bool flag));
+};
+
+TEST(GitRepositoryFactoryTest, CallGitRepositoryImpl)
+{
+    secure_string str;
+    MockGitRepositoryFactory factory;
+    EXPECT_CALL(factory, createRepository("", str, str, false)).Times(AtLeast(1));
+}
 
 int main(int argc, char** argv) 
 {
