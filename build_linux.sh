cc02211911e64f2d1f54c179a44d5c06c0896002
ableigdev <igor.yarovoy.98@ukr.net>
2019-01-19 21:27:52 +0200

    Update an instruction how to build a project on Linux and Windows

--- build_linux.sh
+++ build_linux.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
+conan remote add ableigdev https://api.bintray.com/conan/ableigdev/conan-libgit2
 mkdir build && cd build
-conan install .. 
+conan install .. -s build_type=Release --build libgit2
 cmake .. -G "Unix Makefiles"
 cmake --build .
--- build_windows.sh
+++ build_windows.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
+conan remote add ableigdev https://api.bintray.com/conan/ableigdev/conan-libgit2
 mkdir build && cd build
-conan install .. -s compiler.version=15
+conan install .. -s compiler.version=15 -s build_type=Release --build libgit2
 cmake .. -G "Visual Studio 15 Win64"
 cmake --build . 
