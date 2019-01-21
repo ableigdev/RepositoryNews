939c381067464c93221a97a3f27a4f471ee8d7c5
ableigdev <igor.yarovoy.98@ukr.net>
2019-01-21 12:31:55 +0200

    Update an address of the remote repository

--- build_linux.sh
+++ build_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-conan remote add ableigdev https://api.bintray.com/conan/ableigdev/conan-libgit2
+conan remote add ableigdev https://api.bintray.com/conan/ableigdev/public-conan
 mkdir build && cd build
 conan install .. -s build_type=Release --build libgit2
 cmake .. -G "Unix Makefiles"
--- build_windows.sh
+++ build_windows.sh
@@ -1,5 +1,5 @@
 #!/bin/bash 
-conan remote add ableigdev https://api.bintray.com/conan/ableigdev/conan-libgit2
+conan remote add ableigdev https://api.bintray.com/conan/ableigdev/public-conan
 mkdir build && cd build
 conan install .. -s compiler.version=15 -s build_type=Release --build libgit2
 cmake .. -G "Visual Studio 15 Win64"
