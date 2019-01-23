7ca68af9a126298d0300c1595c69b95d02c38dd0
ableigdev <igor.yarovoy.98@ukr.net>
2019-01-23 12:29:51 +0200

    Add a namespace for the helpers types

--- rpnews/helpers/rpnews_types.h
+++ rpnews/helpers/rpnews_types.h
@@ -4,26 +4,29 @@
 #include "SecureAllocator.h"
 #include <string>
 
-typedef std::basic_string<char, std::char_traits<char>, secure::SecureAllocator<char>> secure_string;
-
-struct commit
+namespace helpers
 {
-    std::string author;
-    std::string message;
-    std::string date_time;
-};
+    typedef std::basic_string<char, std::char_traits<char>, secure::SecureAllocator<char>> secure_string;
 
-enum TypeRepository { Git };
+    struct commit
+    {
+        std::string author;
+        std::string message;
+        std::string date_time;
+    };
 
-struct GetRepositoryInfo
-{
-    std::string path {};
-    int type = -1;
-    secure_string user {};
-    secure_string pass {};
-    std::string branchName {};
-    int branchIndex = -1;
-    int timeInterval = -1;
-};
+    enum TypeRepository { Git };
+
+    struct GetRepositoryInfo
+    {
+        std::string path {};
+        int type = -1;
+        secure_string user {};
+        secure_string pass {};
+        std::string branchName {};
+        int branchIndex = -1;
+        int timeInterval = -1;
+    };
+}
 
 #endif //RPNEWS_RPNEWS_TYPES_H
